91065589af128eb6091a3433eba0306818e304da
FlegerDan <dan_fleger@yahoo.com>
2024-11-20 21:15:49 +0200

    FirstOp
    First operation update, almost done with the string processing

--- BinaryCalculator/BinaryCalculator/FileName.cpp
+++ BinaryCalculator/BinaryCalculator/FileName.cpp
@@ -8,7 +8,10 @@
 #include <stdlib.h>
 
 
-
+void string_allocation(char** string, int length);
+void write_binary_string(char* string, bool which);
+bool binary_validation_string(char* string,int length);
+int size_binary_string(char* string);
 void and2numbers();
 void or2numbers();
 void xor2numbers();
@@ -41,16 +44,88 @@ int main()
 		switch (option)
 		{
 		case 1:
+			and2numbers();
+			system("pause");
 			break;
 		}
 		system("cls");
 	}
 
 }
+void string_allocation(char** string, int length)
+{
+	*string = (char*)(malloc(sizeof(char) * length));
+}
+void write_binary_string(char* string,bool which)
+{
+	int length;
+	do
+	{
+		system("cls");
+		printf("please insert a maximum 32 bit number for example: 11010\n");
+		if (false == which)
+		{
+			printf("please insert first number:");
+		}
+		else
+		{
+			printf("please insert second number:");
+		}
+		scanf("%s", string);
+		length = size_binary_string(string);
+		binary_validation_string(string, length);
+		//printf("\n %d \n", size_binary_string(string));
+	} while (length > 32 || (false==binary_validation_string(string, length)));
+	
 
+}
+bool binary_validation_string(char* string,int length)
+{
+	bool ok = true;
+	for (int i = 0; i < length; i++)
+	{
+		if (string[i] != '1' and string[i] != '0')
+		{
+			ok = false;
+		}
+	}
+	return ok;
+}
+int size_binary_string(char* string)
+{
+	int length=0;
+	while (string[length] != NULL)
+	{
+		length++;
+	}
+	return length;
+}
 void and2numbers()
 {
-
+	char* number1;
+	char* number2;
+	string_allocation(&number1, 32);
+	string_allocation(&number2, 32);
+	
+	write_binary_string(number1,false);
+	write_binary_string(number2,true);
+	
+	printf("%s", number1);
+	printf("%s", number2);
+	/*
+	int number1 = 0u;
+	int number2 = 0u;
+	system("cls");
+	printf("Option & between 2 numbers ( the input will be integer (int) and the result also integer but printed in binary )\n");
+	printf("Please introduce the first number:");
+	scanf("%d", &number1);
+	printf("\n");
+	printf("Please introduce the second number:");
+	scanf("%d", &number2);
+	printf("\n");
+	number1 = number1 & number2;
+	printf("%b", number1);
+	*/
 }
 void or2numbers()
 {
